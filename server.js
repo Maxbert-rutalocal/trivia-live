9244e68caaa738f0f992b59409b8f09232e5a9c9
Maxbert-rutalocal <maxbert83@gmail.com>
2026-08-08 12:15:06 -0500

    fix: servidor anti-cache y actualizacion final

--- server.js
+++ server.js
@@ -6,19 +6,24 @@ const { WebcastPushConnection } = require('tiktok-live-connector');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// 1. Servir archivos estáticos
-app.use(express.static(path.join(__dirname)));
-
+// 1. RUTA ANTI-CACHÉ ABSOLUTA (Debe ir ANTES de los archivos estáticos)
 app.get('/preguntas.json', (req, res) => {
+    res.setHeader('Surrogate-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
     res.sendFile(path.join(__dirname, 'preguntas.json'));
 });
 
-// 2. Crear el servidor HTTP de Express
+// 2. Servir archivos estáticos
+app.use(express.static(path.join(__dirname)));
+
+// 3. Crear el servidor HTTP de Express
 const server = app.listen(PORT, () => {
     console.log(`🚀 Servidor de Trivia Live iniciado en http://localhost:${PORT}`);
 });
 
-// 3. Servidor de WebSockets
+// 4. Servidor de WebSockets
 const wss = new WebSocketServer({ server });
 let clientesActivos = [];
 
@@ -44,7 +49,7 @@ wss.on('connection', (ws) => {
 });
 
 // ==========================================
-// 4. CONEXIÓN DIRECTA A TIKTOK LIVE
+// 5. CONEXIÓN DIRECTA A TIKTOK LIVE
 // ==========================================
 let tiktokUsername = "hubertmarquez0"; 
 let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
