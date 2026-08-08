e200ea3f5a3e3dc98ab19512194e1e7c679cac02
Maxbert-rutalocal <maxbert83@gmail.com>
2026-08-08 10:43:07 -0500

    update: integracion tiktok, control movil y gitignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { WebSocketServer } = require('ws');
 const path = require('path');
+const { WebcastPushConnection } = require('tiktok-live-connector');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,7 +9,6 @@ const PORT = process.env.PORT || 8080;
 // 1. Servir archivos estáticos
 app.use(express.static(path.join(__dirname)));
 
-// RUTA DIRECTA Y OBLIGATORIA PARA EL JSON
 app.get('/preguntas.json', (req, res) => {
     res.sendFile(path.join(__dirname, 'preguntas.json'));
 });
@@ -16,10 +16,9 @@ app.get('/preguntas.json', (req, res) => {
 // 2. Crear el servidor HTTP de Express
 const server = app.listen(PORT, () => {
     console.log(`🚀 Servidor de Trivia Live iniciado en http://localhost:${PORT}`);
-    console.log('Esperando conexiones...');
 });
 
-// 3. Pegar el servidor de WebSockets sobre el mismo puerto HTTP
+// 3. Servidor de WebSockets
 const wss = new WebSocketServer({ server });
 let clientesActivos = [];
 
@@ -41,5 +40,29 @@ wss.on('connection', (ws) => {
 
     ws.on('close', () => {
         clientesActivos = clientesActivos.filter(c => c !== ws);
+    });
+});
+
+// ==========================================
+// 4. CONEXIÓN DIRECTA A TIKTOK LIVE
+// ==========================================
+let tiktokUsername = "hubertmarquez0"; 
+let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
+
+tiktokLiveConnection.connect().then(state => {
+    console.info(`✅ Conectado exitosamente al Live de TikTok: ${state.roomId}`);
+}).catch(err => {
+    console.error('❌ Error conectando a TikTok. ¿Estás en vivo en este momento?', err);
+});
+
+// Escuchar el evento cuando alguien entra al live (Member Join)
+tiktokLiveConnection.on('member', data => {
+    const mensajeSaludo = JSON.stringify({ tipo: 'NUEVO_ESPECTADOR', usuario: data.uniqueId });
+    
+    // Enviar la señal de saludo al tablero admin.html
+    clientesActivos.forEach(cliente => {
+        if (cliente.readyState === 1) {
+            cliente.send(mensajeSaludo);
+        }
     });
 });
